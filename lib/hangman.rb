067c916bafa44fc77034460a453d4183d523c732
Benjamin Pearce <hawk.git@bcpearce.com>
2014-10-02 22:37:58 -0400

    added function for generating random word

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,19 @@
-class hangman
+class Hangman
+
+  COUNTER = 6
+  DICTIONARY_PATH = "dictionary.txt"
+  @@dictionary = File.readlines(DICTIONARY_PATH)
+
+  def initialize(word_length = (5..12))
+    # word_length is passed as a range which picks how long the word can be
+    possible_words = []
+    @@dictionary.each { |word| possible_words << word if word_length.include?(word.length) }
+    @word_to_guess = possible_words[rand(0...possible_words.length)]
+    @guesses = 0
+  end
+
+  def word_to_guess
+    @word_to_guess
+  end
 
 end
---
+++ spec/hangman_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/hangman.rb'
+
+describe Hangman do 
+
+  before { @hangman = Hangman.new(5..12) }
+
+  it { should respond_to :word_to_guess }
+  it "should pick a word bounded by the range" do
+    expect((5..12).cover?(@hangman.word_to_guess.length)).to be_true
+  end
+  
+end
